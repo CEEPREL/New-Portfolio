59e666bda9ddd0faf3366e1296e39208291af491
Agboola Boluwatife <75367773+CEEPREL@users.noreply.github.com>
2024-03-26 00:36:06 +0100

    Update logo.jsx

--- src/components/common/logo.jsx
+++ src/components/common/logo.jsx
@@ -13,7 +13,7 @@ const Logo = (props) => {
 	}
 
 	const imageElement = (
-		//<img src={INFO.main.logo} alt="logo" className="logo" width={width} />
+		<img src={INFO.main.logo} alt="logo" className="logo" width={width} />
 	);
 
 	return (
