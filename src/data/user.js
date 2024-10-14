e7b0aa5690eaf075bf8d4e4791e192a7b8fa0be8
CEEPREL <ceeprel3@gmail.com>
2024-10-14 09:14:29 +0100

    update

--- src/data/user.js
+++ src/data/user.js
@@ -61,22 +61,22 @@ const INFO = {
 		},
 
 		{
-			title: "Reeplay",
+			title: "Intashop",
 			description:
-				"Reeplay, built using React, is an online reservation platform with menus. It simplifies customer browsing through menus with just a few clicks or by scanning lounge barcodes.",
+				"InstaShop InstaShop is a platform that enables individuals to create their own independent online stores, making it easy for sellers to showcase their products to a wide audience.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://reeplaylounge.com.ng/",
+			link: "https://intashop.vercel.app/",
 		},
 
-		// {;
-		// 	// title: "MusicPlayer",
-		// 	// description:
-		// 	// 	"This is a simple music app built with React, Tailwind, and the RapidAPI Shazam Core API. The app allows users to search for songs, view the song details, and play a preview of the song. The app also provides a list of related songs based on the user's search query.",
-		// 	// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-		// 	// linkText: "View Project",
-		// 	// link: "https://github.com/CEEPREL/music_play",
-		// },
+		{
+			title: "E-Stack",
+			description:
+				"This is a simple E-commerce app built with Next.js and Tailwind CSS. The app showcases features like adding, editing, and uploading product images, as well as searching for products using a unique ID identifier.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://e-stack.vercel.app",
+		},
 	],
 };
 
